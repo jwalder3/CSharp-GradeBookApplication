aa3af24f4c8218c4d5a0bb684808279daf3ec70c
Joseph Walder <joseph.walder@xceptor.com>
2019-10-01 16:24:33 +0100

    RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,10 @@
+﻿namespace Gradebook.Gradebooks
+{
+	public class RankedGradeBook : BaseGradeBook
+	{
+		public RankedGradeBook(string name) : base (name)
+		{
+			Type = Enum.GradeBookType.Ranked;
+		}
+	}
+}
