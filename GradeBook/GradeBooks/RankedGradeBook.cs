827dd8d1806ee1aca0c5baa99e3d7ff5195e6bd6
Joseph Walder <joseph.walder@xceptor.com>
2019-10-08 17:22:04 +0100

    update

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -23,7 +23,7 @@ namespace GradeBook.GradeBooks
 				return 'A';
 			else if (grades[(threshold * 2) - 1] <= averageGrade)
 				return 'B';
-			else if (grades[(threshold * 4) - 1] <= averageGrade)
+			else if (grades[(threshold * 3) - 1] <= averageGrade)
 				return 'C';
 			else if (grades[(threshold * 4) - 1] <= averageGrade)
 				return 'D';
